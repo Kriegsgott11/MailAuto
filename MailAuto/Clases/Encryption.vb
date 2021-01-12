c91b828dec6dbd494fb476843d7d08e68c650ec9
Hernán Xavier Scheffer <hernan.scheffer_11@hotmail.com>
2021-01-12 10:55:39 -0300

    Change encoding to UTF8

--- MailAuto/Clases/Encryption.vb
+++ MailAuto/Clases/Encryption.vb
@@ -15,13 +15,13 @@
         Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
         Dim encrypted As String
         Dim hash(31) As Byte
-        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.UTF8Encoding.UTF8.GetBytes(password))
         Array.Copy(temp, 0, hash, 0, 16)
         Array.Copy(temp, 0, hash, 15, 16)
         AES.Key = hash
         AES.Mode = Security.Cryptography.CipherMode.ECB
         Dim DESEncrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateEncryptor
-        Dim Buffer As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(text)
+        Dim Buffer As Byte() = System.Text.UTF8Encoding.UTF8.GetBytes(text)
         encrypted = Convert.ToBase64String(DESEncrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
         Return encrypted
     End Function
@@ -42,14 +42,14 @@
         Dim Hash_AES As New System.Security.Cryptography.MD5CryptoServiceProvider
         Dim decrypted As String
         Dim hash(31) As Byte
-        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(password))
+        Dim temp As Byte() = Hash_AES.ComputeHash(System.Text.UTF8Encoding.UTF8.GetBytes(password))
         Array.Copy(temp, 0, hash, 0, 16)
         Array.Copy(temp, 0, hash, 15, 16)
         AES.Key = hash
         AES.Mode = Security.Cryptography.CipherMode.ECB
         Dim DESDecrypter As System.Security.Cryptography.ICryptoTransform = AES.CreateDecryptor
         Dim Buffer As Byte() = Convert.FromBase64String(text)
-        decrypted = System.Text.ASCIIEncoding.ASCII.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
+        decrypted = System.Text.UTF8Encoding.UTF8.GetString(DESDecrypter.TransformFinalBlock(Buffer, 0, Buffer.Length))
         Return decrypted
     End Function
 End Class
